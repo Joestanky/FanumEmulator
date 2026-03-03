09c58da0bf1c2caf4176161c880c63a67925a0bf
Joestanky <joeyjoey1046@gmail.com>
2026-03-03 10:41:21 -0500

    i hate
    g

--- compiler.py
+++ compiler.py
@@ -1,11 +1,11 @@
 file = open("code.txt", 'r')
+out = open("code.nes", 'wb')
 code = file.read()
 
 from main import INSSTR
 lines = code.split('\n')
 print(lines)
 
-addressingMode = ''
 
 def toHex(byte):
 	hexes = ['0', '1', '2', '3',
@@ -18,34 +18,41 @@ def toHex(byte):
 
 ByteDump = []
 
+
+addressingMode = ''
+
+#Programs Lines
 for line in lines:
 	sections = line.split(' ')
 	opcode = sections[0]
+	#Non Implied Instructions
 	if len(sections) == 2:
 		fullOperand = sections[1]
-		print(opcode, '|', fullOperand)
 
 		addressingMode = '~~'
+
+		#Addressing Modes
 		if 		'd' == 'f':
 			pass
 		elif 	"#" in fullOperand:
-			print(fullOperand, "bullshit gimme my money")
 			addressingMode = 'IM'
 		elif '(' in fullOperand:
-			if 		'X' in fullOperand: addressingMode = 'IX'
-			elif 	'Y' in fullOperand: addressingMode = 'IY'
+			if 		'X' in fullOperand: addressingMode = 'IX' 	#Indirect X
+			elif 	'Y' in fullOperand: addressingMode = 'IY' 	#Indirect Y
 		elif 	len(fullOperand) >3:
 			addressingMode = 'A'
-			if 		'X' in fullOperand: addressingMode += 'X'
-			elif 	'Y' in fullOperand: addressingMode += 'Y'
-			else: 	addressingMode += 'B'
+			if 		'X' in fullOperand: addressingMode += 'X' 	#Absolute X
+			elif 	'Y' in fullOperand: addressingMode += 'Y' 	#Absolute Y
+			else: 	addressingMode += 'B' 						#Absolute
 		else:
 			addressingMode = 'Z'
-			if 		'X' in fullOperand: addressingMode += 'X'
-			else:	addressingMode += 'P'
-		print(opcode+addressingMode)
+			if 		'X' in fullOperand: addressingMode += 'X' 	#Zero Page X
+			else:	addressingMode += 'P' 						#Zero Page
+
 		opcodeByte = INSSTR.index(opcode+addressingMode)
-		print(hex(opcodeByte))
+		print(opcode+addressingMode, hex(opcodeByte), '|', fullOperand)
+
+		#Operand
 		operandBytes = []
 		if addressingMode in ['AB','AX','AY']: 
 			first = sections[1][3:5]
@@ -56,14 +63,30 @@ for line in lines:
 			operandBytes.append(toHex(sections[1][-2:]))
 		elif addressingMode in ['IX','IY']:
 			operandBytes.append(toHex(sections[1][2:4]))
-		print(operandBytes)
+		opBytes = ''	
+		for i in operandBytes:
+			opBytes += hex(i) +' '
+		print(opBytes)
 
+		#Dumping
 		ByteDump.append(opcodeByte)
 		for byte in operandBytes:
 			ByteDump.append(byte)
 
 
-RawDump = bytearray(0x8000)
+#Header
+Header = [0x4E,0x45,0x53,0x1A] #"NES", then MS-DOS EOF (dunno why EOF there)
+ByteDump.insert(0, Header[0])
+ByteDump.insert(1, Header[1])
+ByteDump.insert(2, Header[2])
+ByteDump.insert(3, Header[3])
+
+
+
+
+ByteDump.append()
+
+RawDump = bytearray(len(ByteDump))
 
 ByteDumpHex = []
 
@@ -71,6 +94,13 @@ for x,byte in enumerate(ByteDump):
 	ByteDumpHex.append(hex(byte))
 	RawDump[x] = byte
 
+print("Byte Dump ---")
 print(ByteDumpHex)
 
 print(RawDump)
+out.write(RawDump)
+print("---")
+
+
+out.close()
+file.close()
